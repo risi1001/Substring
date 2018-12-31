71331fe4f49a5ea17e9d97d5ab9b78d5dadcf019
Ritesh Singh <32819110+risi1001@users.noreply.github.com>
2018-12-31 13:19:43 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,15 +1,15 @@
-def substring(str,n):
+def substring(str,n):  //defining function for substring
     for le in range(1,n+1):
         for i in range(0,n-le+1):
             j=i+le-1
             b = ["\0"]
             for k in range(i,j+1):
                 print(str[k],end="")
-            print("\n")
-t=input()
+            print("\n")        
+t=input()  // t test cases    
 for i in range(int(t)):
-    s=input()
-    substring(s,int(len(s)))
+    s=input()  //taking string
+    substring(s,int(len(s)))  // passing string and it's length
 
 
 
